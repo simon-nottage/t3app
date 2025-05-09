6b85149ccec5d59efcd46298e89461620ed70958
Simon Nottage <simon.nottage@zoho.com>
2025-05-09 11:08:52 +0100

    Minor change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function HomePage() {
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
         <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-          Welcome to <span className="text-[hsl(280,100%,70%)]">T3</span> App
+          Welcome back to <span className="text-[hsl(280,100%,70%)]">T3</span> App
         </h1>
       </div>
     </main>
